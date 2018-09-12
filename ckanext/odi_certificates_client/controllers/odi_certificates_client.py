40f5db9881980009278f5bc130810c6567a77b7a
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-09-12 12:28:21 +1000

    Added debug info. Added requirements.

--- ckanext/odi_certificates_client/controllers/odi_certificates_client.py
+++ ckanext/odi_certificates_client/controllers/odi_certificates_client.py
@@ -2,7 +2,7 @@ from logging import getLogger
 
 import ckan.plugins.toolkit as t
 
-from action.get import odi_certificate_location_head, odi_certificate_get, odi_certificates_get_all
+from ckanext.odi_certificates_client.logic.action.get import odi_certificate_location_head, odi_certificate_get, odi_certificates_get_all
 from ckanext.odi_certificates_client.lib.request import get_package_dict_from_id
 
 log = getLogger(__name__)
@@ -11,7 +11,7 @@ log = getLogger(__name__)
 class OdiCertificatesClientController(t.BaseController):
 
     def odi_certificate_get_from_id(self, id):
-        log.debug("entered controller get from id...")
+        log.info("entered controller get from id...")
         data_dict = get_package_dict_from_id(id)
         location_url = self.odi_certificate_location_get(data_dict)
         data = self.odi_certificate_get_from_url(data_dict, location_url)
@@ -19,10 +19,12 @@ class OdiCertificatesClientController(t.BaseController):
         return data
 
     def odi_certificate_location_get_from_id(self, id):
+        log.info("entered controller get location from id...")
         data_dict = get_package_dict_from_id(id)
         return self.odi_certificate_location_get(data_dict)
 
     def odi_certificate_location_get(self, data_dict):
+        log.info("entered controller get location...")
         response = odi_certificate_location_head(t.c, data_dict)
         if not response.headers or not response.headers.location:
             t.abort(404, "No odi certificate location was found.")
@@ -31,12 +33,14 @@ class OdiCertificatesClientController(t.BaseController):
         return location_url
 
     def odi_certificates_get_all(self):
+        log.info("entered controller get all...")
         response = odi_certificates_get_all(t.c)
         if not response.text:
             t.abort(404, "No odi certificates were found.")
         return response
 
     def _odi_certificate_get_from_url(self, data_dict, location_url):
+        log.info("entered controller get from url...")
         response = odi_certificate_get(t.c, data_dict, location_url)
 
         if not response.text:
--- ckanext/odi_certificates_client/logic/action/get.py
+++ ckanext/odi_certificates_client/logic/action/get.py
@@ -17,7 +17,7 @@ def odi_certificate_location_head(context=None, data_dict=None):
     :param data_dict:
     :return:
     """
-
+    log.info("entered api get location head...")
     t.check_access('odi_certificates_client_get', context, data_dict)
 
     request_path = get_request_path_for(context, location_request_path)
@@ -41,7 +41,27 @@ def odi_certificate_get(context=None, data_dict=None, request_path=None):
     :param request_path:
     :return:
     """
+    log.info("entered api get...")
+    t.check_access('odi_certificates_client_get', context, data_dict)
 
+    certs_config = context['odi_certificates_config']
+    response = requests.get(
+        request_path,
+        auth=get_auth(certs_config)
+    )
+    log.debug('response from get is: %s' % response.text)
+    return response
+
+@t.side_effect_free
+def odi_certificate_get_from_id(context=None, data_dict=None, request_path=None):
+    """
+
+    :param context:
+    :param data_dict:
+    :param request_path:
+    :return:
+    """
+    log.info("entered api get...")
     t.check_access('odi_certificates_client_get', context, data_dict)
 
     certs_config = context['odi_certificates_config']
@@ -61,6 +81,7 @@ def odi_certificates_get_all(context=None):
     :param request_path:
     :return:
     """
+    log.info("entered controller get all...")
     t.check_access('odi_certificates_client_get_all', context)
 
     request_path = get_request_path_for(context, campaigns_feed_request_path)
