fd0caa9e43dd65be576f6b0d29cb2aeea5043210
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-09-20 16:33:38 +1000

    check nose installation.

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -4,6 +4,8 @@ echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" |
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
+nosetests --version
+
 nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
@@ -11,4 +13,4 @@ nosetests --ckan \
           --cover-package=ckanext.odi_certificates_client \
           --cover-inclusive \
           --cover-erase \
-          --cover-tests
+          --cover-tests ckanext/ckanext-odi_certificates_client
