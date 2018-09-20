046423f127174f6cd1baf8c27ae06ee3cc6a7f65
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-09-20 17:11:16 +1000

    debug travis.

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+pwd
+ls ckan/ckan
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
